cf166fcea02d6e2d70eeac8301d42c36e84bdcb4
WeakUp <916149034@qq.com>
2017-09-17 14:08:23 +0800

    "Activity Life"

---
+++ app/src/main/java/com/example/lisiyan/android_tutorial/SecondActivity.java
@@ -0,0 +1,46 @@
+package com.example.lisiyan.android_tutorial;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class SecondActivity extends AppCompatActivity {
+
+    private final String Tag = "SecondActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        Log.d(Tag,"onCreate");
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(Tag,"onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(Tag,"onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(Tag,"onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(Tag,"onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(Tag,"onDestroy");
+    }
+}
